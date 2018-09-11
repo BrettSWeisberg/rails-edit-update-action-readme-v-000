798a66623fb44cc5baba0896d135dd20202ca511
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-11 23:14:22 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    
+
     @post = Post.find(params[:id])
-    @post.update(params.require(:posts))
+    @post.update(params.require(:post))
     redirect_to post_path(@post)
 
   end
