79ba051d93c520b15172361399a90e0f65be26a5
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-11 23:13:39 +0000

    automatically backed up by learn

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    binding.pry
+    @post = Post.find(params[:id])
+    @post.update(params.require(:posts))
+    redirect_to post_path(@post)
+
+  end
+end
